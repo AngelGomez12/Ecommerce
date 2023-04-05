2048bda7013d64480b1dca94fc8111eafa9eb515
Angel Gomez <106106151+AngelGomez12@users.noreply.github.com>
2023-04-04 23:36:40 -0300

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Header = () => {
+    return (
+        <div className='bg-blue-500'>
+            <h1 className='text-red-500'>Header</h1>
+        </div>
+    )
+}
